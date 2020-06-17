073a8fa794e1197012e2f2dd160be21d5e0ad3d8
venkat <venkatavaradan.r@gmail.com>
2020-06-17 20:51:45 +0530

    fixed simple bug in models.py. created api.py and serializers.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # poke_api
+
 Pokemon DB extension. This is a rest API created using the Django framework
--- api_app/models.py
+++ api_app/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-
 # Create your models here.
 
 class pokemon(models.Model):
-    pokedex_no = models.IntegerField(default = 0, unique = True)
-    name = models.CharField(max_length=25, unique = True)
-    is_legendary = models.BinaryField(unique = True)
+    pokedex_id = models.IntegerField(default = 0, unique = True)
+    pkmn_name = models.CharField(max_length=25, unique = True)
+    is_legendary = models.BooleanField()
     
     def photo_upload_path(self,filename):
         path = f"api_app/uploads/photos/pokemon/{filename}"
         return path
     
     picture = models.ImageField(upload_to = photo_upload_path, null = True, blank = True)
+
+    def __str__(self):
+        return f"{self.pkmn_name} - {self.pokedex_id}"
