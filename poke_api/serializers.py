ad56adae342177d03907c597a155b6f48b5fc170
venkat <venkatavaradan.r@gmail.com>
2020-06-17 21:24:02 +0530

    added pokemon serializer

--- poke_api/serializers.py
+++ poke_api/serializers.py
@@ -1,1 +1,7 @@
-.
+from rest_framework import serializers
+from api_app.models import pokemon
+
+class pokemonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model:pokemon
+        fields '__all__'
