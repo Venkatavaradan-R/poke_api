bb4432f3889ebaf220f090eb2cdaad44fbaff784
venkat <venkatavaradan.r@gmail.com>
2020-06-17 10:58:05 +0530

    added pokemon to models.py

--- api_app/models.py
+++ api_app/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class pokemon(models.Model):
+    pokedex_no = models.IntegerField(default = 0, unique = True)
+    name = models.CharField(max_length=25, unique = True)
+    is_legendary = models.BinaryField(unique = True)
+    
+    def photo_upload_path(self,filename):
+        path = "api_app/uploads/photos/pokemon/{}".format(filename)
+        return path
+    
+    picture = models.ImageField(upload_to = photo_upload_path, null = True, blank = True)
