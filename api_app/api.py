2bd51f8842e3565b1e519c6324a68b73d88916a4
venkat <venkatavaradan.r@gmail.com>
2020-06-21 13:11:13 +0530

    added delete for moves

--- api_app/api.py
+++ api_app/api.py
@@ -88,3 +88,8 @@ class movesDetails(APIView):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request,pk):
+        model = self.get_move(pk)
+        model.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
